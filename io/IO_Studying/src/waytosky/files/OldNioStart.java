ec37ce9ce8e800897c1a232f766072ccf1ff37f2
ajratb <ajratb@ya.ru>
2017-09-17 21:51:14 +0700

    some changes

--- io/IO_Studying/src/waytosky/files/OldNioStart.java
+++ io/IO_Studying/src/waytosky/files/OldNioStart.java
@@ -20,16 +20,17 @@ public class OldNioStart {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        File txtFile = new File("d:\\123.txt");
-        System.out.println("Abs Path : "+txtFile.getAbsolutePath());
+        File file = new File("d:\\123.txt");
+        System.out.println("Abs Path : "+file.getAbsolutePath());
         try {
-            System.out.println("Canonical Path : "+txtFile.getCanonicalPath());
+            System.out.println("Canonical Path : "+file.getCanonicalPath());
         } catch (IOException ex) {
             System.out.println("catch IOException during canonical path getting");
             Logger.getLogger(OldNioStart.class.getName()).log(Level.SEVERE, null, ex);
         }
-        System.out.println("Get Path : "+txtFile.getPath());
+        System.out.println("Get Path : "+file.getPath());
         System.out.println("");
+        System.out.println("file is exist?: "+ file.exists());
     }
     
 }
