8c38874983dd49a8cdd8661d34924d4f5c69711e
ajratb <ajratb@ya.ru>
2017-10-10 23:10:04 +0700

    files

---
+++ io/Files/src/ru/waytosky/FilesApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.waytosky;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Ayrat
+ */
+public class FilesApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        FilesUtils_J8.deleteInDirectory("C:\\123");
+    }
+    
+}
