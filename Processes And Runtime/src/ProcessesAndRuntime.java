6c264099d943f3949d713051dd4cb1230ff8e01f
ajratb <ajratb@ya.ru>
2017-12-12 22:28:32 +0700

    Processes And Runtime project was added

--- Processes And Runtime/src/ProcessesAndRuntime.java
+++ Processes And Runtime/src/ProcessesAndRuntime.java
@@ -15,7 +15,7 @@ public class ProcessesAndRuntime {
     public static void main(String[] args) {
         try {
             // TODO code application logic here
-//        ProcessBuilder pBuilder = new ProcessBuilder
+            // ProcessBuilder pBuilder = new ProcessBuilder
             Runtime.getRuntime().exec("javaw -jar CSS.jar");
         } catch (IOException ex) {
             Logger.getLogger(ProcessesAndRuntime.class.getName()).log(Level.SEVERE, null, ex);
