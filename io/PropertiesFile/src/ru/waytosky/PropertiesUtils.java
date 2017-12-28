9248e476a56616dd4cca18ff4ad13e0f458c5a3a
ayrat <ayrat@nok2-test1>
2017-12-28 19:04:56 +0700

    add propUtils class for experiment

---
+++ io/PropertiesFile/src/ru/waytosky/PropertiesUtils.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.waytosky;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ *
+ * @author ayrat
+ */
+public class PropertiesUtils {
+
+    private static final String propsLocation = System.getProperty("user.home") + "\\AppData\\Roaming\\ИСС Канцелярия\\app.properties";
+    private static Properties props;
+//    private InputStream in;
+//    private OutputStream out;
+
+    public static String getPropertyByName(String name) throws IOException, FileNotFoundException {
+        props = new Properties();
+        File file = new File(propsLocation);
+        if (!file.exists()) {
+            throw new FileNotFoundException("Properties file not found");
+        }
+        try (InputStream in = new FileInputStream(file)) {
+            props.load(in);
+            return props.getProperty(name);
+        }
+    }
+
+    public static void persistProperty(String name, String value) throws IOException, FileNotFoundException {
+        props = new Properties();
+        File file = new File(propsLocation);
+        if (!file.exists()) {
+            throw new FileNotFoundException("Properties file not found");
+        }
+        try (InputStream in = new FileInputStream(file)) {
+            props.load(in);
+           
+        }
+        try (FileOutputStream output = new FileOutputStream(file)) {
+           
+            props.setProperty(name, value);
+            props.store(output, null);
+//            return true;
+        }
+    }
+
+    public static void persistProperties(Map<String, String> propsMap) throws IOException, FileNotFoundException {
+        props = new Properties();
+        File file = new File(propsLocation);
+        if (!file.exists()) {
+            throw new FileNotFoundException("Properties file not found");
+        }
+        try (FileOutputStream output = new FileOutputStream(file)) {
+            for(Map.Entry<String,String> entry: propsMap.entrySet()){
+                props.setProperty(entry.getKey(), entry.getValue());
+            }
+            props.store(output, null);
+        }
+        
+    }
+
+}
